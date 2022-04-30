b73679aed339cf4d11568bc92440820ee3fe9475
Oleg Koval <gevin.gl@gmail.com>
2022-04-30 14:10:51 +0300

    Added cartItem feature + some homework )

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,29 @@
 import "./cart-dropdown.styles.scss";
+
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
+
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+
+import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
+  let navigate = useNavigate();
+  const navigateToCheckOut = () => {
+    navigate(`/checkout`);
+  };
+
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items">CartDropdown item</div>
-      <Button>GO TO CHECKOUT</Button>
+      <div className="cart-items">
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} cartItem={item} />;
+        })}
+      </div>
+      <Button onClick={navigateToCheckOut}>GO TO CHECKOUT</Button>
     </div>
   );
 };
--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,14 +1,113 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+  //find if cartItems contains product to add
+
+  /* //---------my solution  
+  let index = cartItems.findIndex((cartItem) => cartItem.id === productToAdd.id);
+  if (index !== -1) {
+    cartItems[index].quantity++;
+  } else {
+    cartItems.push({ ...productToAdd, quantity: 1 });
+  }
+  return cartItems;*/
+
+  // teacher
+
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  cartCount: 0,
+  modifyCartItemsOnPosition: () => {},
+  cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
-  const value = { isCartOpen, setIsCartOpen };
+  /** Will recive click on some product cart in shop and do:
+   *
+   * if product exist in cartDropdown add ++ to it
+   * else create new entry to array of product in cartdropdown array
+   *
+   */
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => (total += cartItem.quantity),
+      0
+    );
+
+    const newCartTotal = cartItems.reduce(
+      (total, carditem) => (total += carditem.quantity * carditem.price),
+      0
+    );
+
+    setCartCount(newCartCount);
+    setCartTotal(newCartTotal);
+  }, [cartItems]);
+
+  //------------------- MY
+  const modifyCartItemsOnPosition = (id, valueToAdd = 0, remove = false) => {
+    const removeItem = (id) => {
+      const modifiedCartItems = cartItems.filter((item) => item.id !== id); //return all NOT equal to id from argumet
+      setCartItems(modifiedCartItems);
+    };
+
+    //
+    if (remove) {
+      removeItem(id);
+      return;
+    }
+
+    const modifiedCartItems = cartItems.map((item) => {
+      if (item.id === id) {
+        item.quantity += valueToAdd;
+      }
+      return item;
+    });
+    const checkedForZeroes = modifiedCartItems.filter((item) => item.quantity !== 0);
+    console.log(checkedForZeroes);
+    setCartItems(checkedForZeroes);
+  };
+
+  // useEffect(() => {
+  //   const filteredForZeroes = cartItems.filter((item) => item.quantity !== 0);
+  //   setCartItems(filteredForZeroes);
+  // }, [cartItems]);
+
+  //------------------- MY
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    cartCount,
+    modifyCartItemsOnPosition,
+    cartTotal,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
