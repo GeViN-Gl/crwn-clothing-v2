ba6d0fbbcab8cbda737bf6ae23d179f3b5d89c76
Oleg Koval <gevin.gl@gmail.com>
2022-05-02 23:54:51 +0300

    fix routes error

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -24,13 +24,13 @@ const categories = [
     id: 4,
     title: "womens",
     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    route: `shop/womans`,
+    route: `shop/womens`,
   },
   {
     id: 5,
     title: "mens",
     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    route: `shop/mans`,
+    route: `shop/mens`,
   },
 ];
 
